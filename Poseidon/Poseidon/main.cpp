3a97f5632bc7e5d5fbf783673b8c4d16d9d35634
hanneysaurus <hanneysaurus@hotmail.com>
2020-04-21 14:14:30 +0200

    Hello World

---
+++ Poseidon/Poseidon/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// --------------------------------------------------
+// GLOBAL VARIABLES
+
+
+
+// --------------------------------------------------
+// FUNCTION DECLARATIONS
+
+
+
+int main(int argc, char* argv[]){
+
+	std::cout << "Hello World" << std::endl;
+	
+}
